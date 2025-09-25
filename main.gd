df0ea39faa82f8990c9d396cab9beeab56d8b1af
Paraross <adrago@onet.pl>
2025-09-25 18:34:20 +0200

    Build input UI dynamically based on current task

--- main.gd
+++ main.gd
@@ -3,17 +3,34 @@ extends Node
 @onready var shape_world: ShapeWorld = $HBoxContainer/ShapeViewportContainer/ShapeViewport/ShapeWorld
 
 @onready var task_data_grid: GridContainer = $HBoxContainer/PanelContainer/VBoxContainer/TaskDataGrid
-@onready var side_spin_box: SpinBox = task_data_grid.get_node("SideSpinBox")
-@onready var area_spin_box: SpinBox = task_data_grid.get_node("AreaSpinBox")
-@onready var volume_spin_box: SpinBox = task_data_grid.get_node("VolumeSpinBox")
+
+@onready var task_answer_grid: GridContainer = $HBoxContainer/PanelContainer/VBoxContainer/TaskAnswerGrid
+@onready var area_spin_box: SpinBox = task_answer_grid.get_node("AreaSpinBox")
+@onready var volume_spin_box: SpinBox = task_answer_grid.get_node("VolumeSpinBox")
 
 @onready var check_answer_button: Button = $HBoxContainer/PanelContainer/VBoxContainer/CheckAnswerButton
 @onready var new_task_button: Button = $HBoxContainer/PanelContainer/VBoxContainer/NewTaskButton
 
 func _on_button_pressed() -> void:
 	shape_world.spawn_new_task()
-	$HBoxContainer/PanelContainer/VBoxContainer/TaskDataGrid/SideLabel.text = shape_world.current_task.values()[0][0]
-	side_spin_box.value = shape_world.current_task.values()[0][1]
+	
+	for child in task_data_grid.get_children():
+		assert(child is Label or child is SpinBox)
+		child.queue_free()
+	
+	for value_pair: Array in shape_world.current_task.values():
+		var value_name: String = value_pair[0]
+		var value_value: float = value_pair[1]
+		
+		var label := Label.new()
+		label.text = value_name
+		task_data_grid.add_child(label)
+		
+		var spin_box := SpinBox.new()
+		spin_box.editable = false
+		spin_box.step = 0.1
+		spin_box.value = value_value
+		task_data_grid.add_child(spin_box)
 
 
 func _on_check_answer_button_pressed() -> void:
@@ -41,3 +58,5 @@ func _on_check_answer_button_pressed() -> void:
 	else:
 		print("incorrect volume")
 		print(shape_world.current_task.volume_tip())
+	
+	print()
