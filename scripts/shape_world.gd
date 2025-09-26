c283f0e69dd360f7d8ed8492e7fa75b666d1723d
Paraross <adrago@onet.pl>
2025-09-26 10:26:12 +0200

    Move scenes to their own folder

--- scripts/shape_world.gd
+++ scripts/shape_world.gd
@@ -4,7 +4,7 @@ extends WorldEnvironment
 var current_task: Task
 
 func spawn_new_task() -> void:
-	var task_scene: PackedScene = load("res://cube_task.tscn")
+	var task_scene: PackedScene = load("res://scenes/cube_task.tscn")
 	var task: CubeTask = task_scene.instantiate()
 	task.randomize_values()
 	
