3bed5f89cd624b1c31ff8b3113e35e2f4553558c
Paraross <adrago@onet.pl>
2025-09-27 17:42:39 +0200

    Make get new task button also get PrismTask

--- scripts/shape_world.gd
+++ scripts/shape_world.gd
@@ -3,10 +3,22 @@ extends WorldEnvironment
 
 var current_task: Task
 
+# TODO: move to own directory and load all from it
+const ALL_TASKS: Array[NodePath] = [
+	"res://scenes/cube_task.tscn",
+	"res://scenes/prism_task.tscn",
+]
+
 func spawn_new_task() -> void:
-	var task_scene: PackedScene = load("res://scenes/cube_task.tscn")
-	var task: CubeTask = task_scene.instantiate()
+	var random_task_index := randi_range(0, ALL_TASKS.size() - 1)
+	
+	var task_scene: PackedScene = load(ALL_TASKS[random_task_index])
+
+	var task: Task = task_scene.instantiate()
 	task.randomize_values()
+
+	if current_task != null:
+		current_task.queue_free()
 	
 	add_child(task)
 	current_task = task
