3a4aab73aeaedb1d610ff7b208668dd8c58cfb78
Paraross <adrago@onet.pl>
2025-09-27 18:09:10 +0200

    Load all task scenes dynamically from their directory

--- scripts/shape_world.gd
+++ scripts/shape_world.gd
@@ -1,18 +1,24 @@
 class_name ShapeWorld
 extends WorldEnvironment
 
+const TASK_SCENE_DIRECTORY: String = "res://scenes/tasks/"
+
 var current_task: Task
 
-# TODO: move to own directory and load all from it
-const ALL_TASKS: Array[NodePath] = [
-	"res://scenes/tasks/cube_task.tscn",
-	"res://scenes/tasks/prism_task.tscn",
-]
+var all_task_paths: PackedStringArray = []
+
+func _ready() -> void:
+	var task_paths := ResourceLoader.list_directory(TASK_SCENE_DIRECTORY)
+	for path in task_paths:
+		if path.ends_with(".tscn"):
+			all_task_paths.push_back(path)
+
 
 func spawn_new_task() -> void:
-	var random_task_index := randi_range(0, ALL_TASKS.size() - 1)
-	
-	var task_scene: PackedScene = load(ALL_TASKS[random_task_index])
+	var random_task_index := randi_range(0, all_task_paths.size() - 1)
+
+	var task_path := TASK_SCENE_DIRECTORY.path_join(all_task_paths[random_task_index])
+	var task_scene: PackedScene = load(task_path)
 
 	var task: Task = task_scene.instantiate()
 	task.randomize_values()
