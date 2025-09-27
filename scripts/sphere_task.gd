956e92af80bd2afc494f74d74010214497ff71d9
Paraross <adrago@onet.pl>
2025-09-27 18:38:54 +0200

    Create SphereTask

---
+++ scripts/sphere_task.gd
@@ -0,0 +1,37 @@
+extends Task
+
+@export var radius: float = 0.5
+
+@onready var sphere: MeshInstance3D = $Sphere
+@onready var sphere_mesh: SphereMesh = sphere.mesh
+
+func _ready() -> void:
+	sphere_mesh.radius = radius
+	sphere_mesh.height = 2.0 * radius
+
+
+func values() -> Array[Array]:
+	return [["Radius", radius]]
+
+
+func correct_area() -> float:
+	var result := 4.0 * PI * radius * radius
+	return truncate(result)
+
+
+func correct_volume() -> float:
+	var result := 4.0 / 3.0 * PI * radius * radius * radius
+	return truncate(result)
+
+
+func area_tip() -> String:
+	return ""
+
+
+func volume_tip() -> String:
+	return ""
+
+
+func randomize_values() -> void:
+	var rand_value := randf_range(min_value, max_value) / 2.0
+	radius = truncate_round(rand_value)
