d7154556a6aeded5a9cb86c47e34b826d88b5775
Paraross <adrago@onet.pl>
2025-10-08 11:30:27 +0200

    Show a tip button and popup when entering incorrect answer

--- scripts/screens/task_screen.gd
+++ scripts/screens/task_screen.gd
@@ -5,13 +5,25 @@ var available_tasks: Array[Task]
 
 @onready var shape_world: ShapeWorld = $"HBoxContainer/ShapeViewportContainer/ShapeViewport/ShapeWorld"
 @onready var task_vbox: VBoxContainer = $"HBoxContainer/PanelContainer/VBoxContainer"
+
 @onready var task_data_grid: GridContainer = task_vbox.get_node("TaskDataGrid")
 @onready var task_answer_grid: GridContainer = task_vbox.get_node("TaskAnswerGrid")
+
 @onready var area_spin_box: SpinBox = task_answer_grid.get_node("AreaSpinBox")
 @onready var volume_spin_box: SpinBox = task_answer_grid.get_node("VolumeSpinBox")
-@onready var area_result_label: Label = task_answer_grid.get_node("AreaResultLabel")
-@onready var volume_result_label: Label = task_answer_grid.get_node("VolumeResultLabel")
+
+@onready var area_result_hbox: HBoxContainer = task_answer_grid.get_node("AreaResultHbox")
+@onready var area_result_label: Label = area_result_hbox.get_node("Label")
+@onready var area_result_tip_button: Button = area_result_hbox.get_node("Button")
+
+@onready var volume_result_hbox: HBoxContainer = task_answer_grid.get_node("VolumeResultHbox")
+@onready var volume_result_label: Label = volume_result_hbox.get_node("Label")
+@onready var volume_result_tip_button: Button = volume_result_hbox.get_node("Button")
+
 @onready var check_answer_button: Button = task_vbox.get_node("CheckAnswerButton")
+
+@onready var tip_popup: PopupPanel = $TipPopup
+@onready var tip_popup_label: Label = $TipPopup/TipLabel
 
 func on_entered() -> void:
 	var answer_step := 1.0 / 10.0 ** Settings.answer_precision
@@ -26,6 +38,10 @@ func on_left() -> void:
 func reset() -> void:
 	area_spin_box.value = 0.0
 	volume_spin_box.value = 0.0
+	area_result_label.text = ""
+	volume_result_label.text = ""
+	area_result_tip_button.visible = false
+	volume_result_tip_button.visible = false
 	clear_task_data_grid()
 	shape_world.reset_current_task()
 	check_answer_button.disabled = true
@@ -48,15 +64,17 @@ func set_answer_labels() -> void:
 	# area
 	if is_equal_approx(entered_area, correct_area):
 		area_result_label.text = "The area is correct"
+		area_result_tip_button.visible = false
 	else:
 		area_result_label.text = "The area is incorrect"
-		print(shape_world.current_task.area_tip())
+		area_result_tip_button.visible = true
 	# volume
 	if is_equal_approx(entered_volume, correct_volume):
 		volume_result_label.text = "The volume is correct"
+		volume_result_tip_button.visible = false
 	else:
 		volume_result_label.text = "The volume is incorrect"
-		print(shape_world.current_task.volume_tip())
+		volume_result_tip_button.visible = true
 
 	if OS.is_debug_build():
 		var correct_answer_str := " (%s)" 
@@ -88,3 +106,13 @@ func _on_new_task_button_pressed() -> void:
 		spin_box.step = 1.0 / 10.0 ** Settings.data_precision
 		spin_box.value = value_value
 		task_data_grid.add_child(spin_box)
+
+
+func _on_area_result_tip_button_pressed() -> void:
+	tip_popup_label.text = shape_world.current_task.area_tip()
+	tip_popup.show()
+
+
+func _on_volume_result_tip_button_pressed() -> void:
+	tip_popup_label.text = shape_world.current_task.volume_tip()
+	tip_popup.show()
