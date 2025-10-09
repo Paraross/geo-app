aae14e2061f909361f857f519ce6c7bce9954823
Paraross <adrago@onet.pl>
2025-10-09 18:39:30 +0200

    Store settings in a file

--- scripts/screens/settings_screen.gd
+++ scripts/screens/settings_screen.gd
@@ -1,22 +1,50 @@
 class_name SettingsScreen
 extends Screen
 
+var data_precision: Setting = Setting.new()
+var answer_precision: Setting = Setting.new()
+
 @onready var settings_container: VBoxContainer = $CenterContainer/PanelContainer/VBoxContainer
 @onready var data_precision_spin_box: SpinBox = settings_container.get_node("DataPrecision/SpinBox")
 @onready var answer_precision_spin_box: SpinBox = settings_container.get_node("AnswerPrecision/SpinBox")
 
 func on_entered() -> void:
-	data_precision_spin_box.value = Settings.data_precision
-	answer_precision_spin_box.value = Settings.answer_precision
+	data_precision.set_original_value(Settings.data_precision)
+	answer_precision.set_original_value(Settings.answer_precision)
+	data_precision_spin_box.value = data_precision.original_value
+	answer_precision_spin_box.value = answer_precision.original_value
 
 
 func on_left() -> void:
-	pass
+	var all_settings := [
+		data_precision,
+		answer_precision,
+	]
+
+	var settings_changed := all_settings.any(func (setting: Setting) -> bool: return setting.changed)
+
+	if settings_changed:
+		Settings.save_settings_to_file()
 
 
 func _on_data_precision_changed(value: float) -> void:
+	data_precision.set_value(int(value))
 	Settings.data_precision = int(value)
 
 
 func _on_answer_precision_changed(value: float) -> void:
+	answer_precision.set_value(int(value))
 	Settings.answer_precision = int(value)
+
+
+class Setting:
+	var original_value: Variant
+	var changed: bool
+
+	func set_value(value: Variant) -> void:
+		changed = value != original_value
+
+
+	func set_original_value(value: Variant) -> void:
+		original_value = value
+		changed = false
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,7 +1,37 @@
 extends Node
 
+const config_file_path: String = "user://settings.cfg"
+const settings_section: String = "settings"
+
+# TODO: This should probably just be a Dictionary
 var data_precision: int = 1
 var answer_precision: int = 2
 
 var default_task_data_min_value: float = 1.0
 var default_task_data_max_value: float = 2.0
+
+func _ready() -> void:
+	load_settings_from_file()
+
+
+func load_settings_from_file() -> void:
+	var config := ConfigFile.new()
+	var err := config.load(config_file_path)
+
+	if err == Error.ERR_FILE_NOT_FOUND:
+		save_settings_to_file()
+		return
+
+	if err != OK:
+		print("error opening settings file: %s" % err)
+		return
+
+	data_precision = config.get_value(settings_section, "data_precision")
+	answer_precision = config.get_value(settings_section, "answer_precision")
+
+
+func save_settings_to_file() -> void:
+	var config := ConfigFile.new()
+	config.set_value(settings_section, "data_precision", data_precision)
+	config.set_value(settings_section, "answer_precision", answer_precision)
+	config.save(config_file_path)
