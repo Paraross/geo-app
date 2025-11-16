b6ae14cda417cf6c7aa4dbe85ecc25ca888f3f9b
Paraross <adrago@onet.pl>
2025-11-16 17:52:43 +0100

    Make TaskFilterScreen elements not resize all the time

--- scripts/screens/task_filter_screen.gd
+++ scripts/screens/task_filter_screen.gd
@@ -96,3 +96,11 @@ func _on_task_list_item_clicked(index: int, _at_position: Vector2, mouse_button_
 		task_settings_popup_grid.add_child(max_spin_box)
 
 	task_settings_popup.show()
+
+
+func _on_task_list_resized() -> void:
+	# disable auto-resizing after size is set for the first time
+	if task_list.size.x != 0.0 and task_list.size.y != 0.0:
+		task_list.custom_minimum_size = task_list.size
+		task_list.auto_width = false
+		task_list.auto_height = false
