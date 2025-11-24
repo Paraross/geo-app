8061e7658e66f4434224f54f67d47ed9bc9e2408
Paraross <adrago@onet.pl>
2025-11-24 13:39:14 +0100

    Add raycast to ShapeWorld for object picking

--- scripts/shape_world.gd
+++ scripts/shape_world.gd
@@ -3,6 +3,22 @@ extends WorldEnvironment
 
 var current_task: Task
 
+@onready var camera: Camera3D = $Camera
+@onready var raycast: RayCast3D = $Camera/RayCast
+@onready var viewport: SubViewport = get_parent()
+
+
+# TODO: disable process for all background screens
+# TODO: better screen management, SceneTree set_current_scene?
+# TODO: make figures scenes, add collision shapes
+func _process(_delta: float) -> void:
+	var viewport_size := viewport.size
+	var mouse_pos := viewport.get_mouse_position()
+	var mouse_in_viewport := mouse_pos.x >= 0.0 and mouse_pos.x <= viewport_size.x and mouse_pos.y >= 0.0 and mouse_pos.y <= viewport_size.y
+
+	raycast.target_position = 10.0 * camera.project_local_ray_normal(mouse_pos)
+	raycast.enabled = mouse_in_viewport
+
 
 func spawn_new_task(available_tasks: Array[Task]) -> void:
 	var random_task_index := randi_range(0, available_tasks.size() - 1)
