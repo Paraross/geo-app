9dd7159f0675abd9290f04a228d1ea34ec4af671
Paraross <adrago@onet.pl>
2025-12-09 16:48:46 +0100

    Show if answer is correct in task screen Clean up the file

--- scripts/screens/task_screen.gd
+++ scripts/screens/task_screen.gd
@@ -42,69 +42,16 @@ func reset() -> void:
 	shape_world.reset_current_task()
 	check_answer_button.disabled = true
 	current_step = 0
-	if step_nav_hbox != null:
-		step_nav_hbox.visible = false
 
 
 func update_step_nav_ui() -> void:
 	var task := shape_world.current_task
-	var has_task := task != null
-	step_nav_hbox.visible = has_task
-
-	if not has_task:
-		return
 
 	var steps := task.step_count()
 	current_step = clamp(current_step, 0, max(0, steps - 1))
 	step_title_label.text = task.step_title(current_step)
 	prev_step_button.disabled = current_step <= 0
 	next_step_button.disabled = current_step >= steps - 1
-
-
-func clear_results() -> void:
-	pass
-	# area_result_label.text = ""
-	# volume_result_label.text = ""
-	# area_result_tip_button.visible = false
-	# volume_result_tip_button.visible = false
-
-
-func set_answer_labels() -> void:
-	var task := shape_world.current_task
-
-	if task == null:
-		return
-
-	# var is_volume_step := current_step == task.step_count() - 1
-	#
-	# var correct_answer := Global.round_task_answer(task.correct_answer_for_step(current_step))
-
-	# if not is_volume_step:
-	# 	var entered_area := area_spin_box.value
-	# 	if is_equal_approx(entered_area, correct_answer):
-	# 		area_result_label.text = "The answer is correct"
-	# 		area_result_tip_button.visible = false
-	# 	else:
-	# 		area_result_label.text = "The answer is incorrect"
-	# 		area_result_tip_button.visible = true
-	# 	if OS.is_debug_build():
-	# 		var correct_answer_str := " (%s)"
-	# 		area_result_label.text += correct_answer_str % correct_answer
-	# 	# Hide volume result on area steps
-	# 	volume_result_hbox.visible = false
-	# else:
-	# 	var entered_volume := volume_spin_box.value
-	# 	if is_equal_approx(entered_volume, correct_answer):
-	# 		volume_result_label.text = "The volume is correct"
-	# 		volume_result_tip_button.visible = false
-	# 	else:
-	# 		volume_result_label.text = "The volume is incorrect"
-	# 		volume_result_tip_button.visible = true
-	# 	if OS.is_debug_build():
-	# 		var correct_answer_str2 := " (%s)"
-	# 		volume_result_label.text += correct_answer_str2 % correct_answer
-	# 	# Hide area result on volume step
-	# 	area_result_hbox.visible = false
 
 
 func get_new_task() -> void:
@@ -161,10 +108,18 @@ func set_step_ui() -> void:
 		var answer_spinbox := SpinBox.new()
 		answer_spinbox.step = 1.0 / 10.0 ** Settings.answer_precision
 		answer_spinbox.value = step.correct_answer()
+		answer_spinbox.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+
+		var answer_mark_label := Label.new()
+		answer_mark_label.visible = false
+		
+		var answer_hbox := HBoxContainer.new()
+		answer_hbox.add_child(answer_spinbox)
+		answer_hbox.add_child(answer_mark_label)
 
 		var step_vbox := VBoxContainer.new()
 		step_vbox.add_child(title_hbox)
-		step_vbox.add_child(answer_spinbox)
+		step_vbox.add_child(answer_hbox)
 
 		steps_vbox.add_child(step_vbox)
 
@@ -173,9 +128,13 @@ func set_step_ui() -> void:
 
 func update_step_ui() -> void:
 	var i := 0
-	# only elements related to current and earlier steps are visible
 	for step_container: Container in steps_vbox.get_children():
 		step_container.visible = i <= current_step
+
+		var answer_hbox: HBoxContainer = step_container.get_child(1)
+		var answer_spinbox: SpinBox = answer_hbox.get_child(0)
+		answer_spinbox.editable = i == current_step
+
 		i += 1
 
 
@@ -192,7 +151,6 @@ func _on_volume_result_tip_button_pressed() -> void:
 func _on_prev_step_pressed() -> void:
 	if current_step > 0:
 		current_step -= 1
-		clear_results()
 		update_step_nav_ui()
 		update_step_ui()
 
@@ -201,14 +159,27 @@ func _on_next_step_pressed() -> void:
 	var steps := shape_world.current_task.step_count()
 	if current_step < steps - 1:
 		current_step += 1
-		clear_results()
 		update_step_nav_ui()
 		update_step_ui()
 
 
 func _on_check_answer_button_pressed() -> void:
 	assert(shape_world.current_task != null)
-	set_answer_labels()
+	# set_answer_labels()
+
+	var correct_answer := shape_world.current_task.steps()[current_step].correct_answer()
+
+	var step_container: Container = steps_vbox.get_children()[current_step]
+	var answer_hbox: HBoxContainer = step_container.get_child(1)
+	var answer_spinbox: SpinBox = answer_hbox.get_child(0)
+	var answer_mark_label: Label = answer_hbox.get_child(1)
+
+	var is_answer_correct := is_equal_approx(answer_spinbox.value, correct_answer)
+
+	answer_mark_label.text = "️✅" if is_answer_correct else "❌"
+	answer_mark_label.visible = true
+
+	next_step_button.disabled = not is_answer_correct
 
 
 func _on_new_task_button_pressed() -> void:
