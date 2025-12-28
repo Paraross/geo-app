8737c9eb33b901be4fe54909726795ce3644b9d8
Paraross <adrago@onet.pl>
2025-12-28 13:34:06 +0100

    Allow loading vertices for predefined shapes

--- scripts/screens/playground_screen.gd
+++ scripts/screens/playground_screen.gd
@@ -1,22 +1,51 @@
 class_name PlaygroundScreen
 extends Screen
 
-const VERTICES: PackedVector3Array = [
-	# front
-	Vector3(-1, 1, 1), # 1 left top near
-	Vector3(1, 1, 1), # 2 right top near
-	Vector3(1, -1, 1), # 6 right bottom near
-	Vector3(-1, -1, 1), # 5 left bottom near
-	# back
-	Vector3(-1, 1, -1), # 0 left top far
-	Vector3(1, 1, -1), # 3 right top far
-	Vector3(1, -1, -1), # 7 right bottom far
-	Vector3(-1, -1, -1), # 4 left bottom far
+const SHAPE_OPTIONS: Array[String] = [
+	"Cube",
+	"Prism",
+	"Pyramid",
+]
+
+const SHAPE_VERTICES: Array[PackedVector3Array] = [
+	CUBE_VERTICES,
+	PRISM_VERTICES,
+	PYRAMID_VERTICES,
+]
+
+const CUBE_VERTICES: PackedVector3Array = [
+	Vector3(-1, 1, 1),
+	Vector3(1, 1, 1),
+	Vector3(1, -1, 1),
+	Vector3(-1, -1, 1),
+	Vector3(-1, 1, -1),
+	Vector3(1, 1, -1),
+	Vector3(1, -1, -1),
+	Vector3(-1, -1, -1),
+]
+
+const PRISM_VERTICES: PackedVector3Array = [
+	Vector3(-1, -1, 1),
+	Vector3(1, -1, 1),
+	Vector3(0, 1, 1),
+	Vector3(-1, -1, -1),
+	Vector3(1, -1, -1),
+	Vector3(0, 1, -1),
+]
+
+const PYRAMID_VERTICES: PackedVector3Array = [
+	Vector3(0, 1, 0),
+	Vector3(-1, -1, 1),
+	Vector3(1, -1, 1),
+	Vector3(1, -1, -1),
+	Vector3(-1, -1, -1),
 ]
 
 var vertex_ui_elements: Array[VertexUiElement] = []
 
 @onready var polyhedron_environment: PolyhedronEnvironment = $HBoxContainer/ShapeViewportContainer/ShapeViewport/PolyhedronEnvironment
+
+@onready var shape_button: OptionButton = $HBoxContainer/RightPanel/RightVBox/PredefinedHBox/ShapeButton
 
 @onready var right_vbox: VBoxContainer = $HBoxContainer/RightPanel/RightVBox
 @onready var vertices_vbox: VBoxContainer = right_vbox.get_node("ScrollContainer/VBox/VerticesVBox")
@@ -25,17 +54,17 @@ var vertex_ui_elements: Array[VertexUiElement] = []
 @onready var popup: PopupPanel = $Popup
 
 
+func _ready() -> void:
+	for shape in SHAPE_OPTIONS:
+		shape_button.add_item(shape)
+	shape_button.selected = 0
+
+
 func on_entered() -> void:
 	for vertex_ui_element: VertexUiElement in vertices_vbox.get_children():
 		vertex_ui_element.queue_free()
 		vertices_vbox.remove_child(vertex_ui_element)
 	vertex_ui_elements.clear()
-
-	for v in VERTICES:
-		var new_vertex_ui_element := add_new_vertex_ui_element()
-		new_vertex_ui_element.x_spinbox.value = v.x
-		new_vertex_ui_element.y_spinbox.value = v.y
-		new_vertex_ui_element.z_spinbox.value = v.z
 
 
 func on_left() -> void:
@@ -90,3 +119,19 @@ func _on_create_polyhedron_button_pressed() -> void:
 	var polyhedron := polyhedron_environment.polyhedron
 	polyhedron.vertices = vertices
 	polyhedron.update_vertices()
+
+
+func _on_load_button_pressed() -> void:
+	var vertices := SHAPE_VERTICES[shape_button.selected]
+	print(vertices)
+
+	for vertex_ui_element: VertexUiElement in vertices_vbox.get_children():
+		vertex_ui_element.queue_free()
+		vertices_vbox.remove_child(vertex_ui_element)
+	vertex_ui_elements.clear()
+
+	for v in vertices:
+		var new_vertex_ui_element := add_new_vertex_ui_element()
+		new_vertex_ui_element.x_spinbox.value = v.x
+		new_vertex_ui_element.y_spinbox.value = v.y
+		new_vertex_ui_element.z_spinbox.value = v.z
