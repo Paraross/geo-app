6c83ab75f2bdc7d608dce56a616bacc39d41a61c
Paraross <adrago@onet.pl>
2025-12-31 17:19:12 +0100

    Update sphere task script

--- scripts/tasks/sphere_task.gd
+++ scripts/tasks/sphere_task.gd
@@ -1,6 +1,6 @@
 extends Task
 
-var radius: TaskFloatValue = TaskFloatValue.default() \
+var radius: TaskFloatValue = TaskFloatValue.with_min_max(1.0, 5.0) \
 .with_on_set(func() -> void: sphere.radius = radius.value)
 
 @onready var sphere: Sphere = $Sphere
@@ -27,11 +27,13 @@ func steps() -> Array[Step]:
 		Step.new(
 			"1. Calculate area",
 			"A sphere's area is 4 × π × radius²",
+			2,
 			sphere.area,
 		),
 		Step.new(
 			"2. Calculate volume",
 			"A sphere's volume is (4/3) × π × radius³",
+			2,
 			sphere.volume,
 		),
 	]
