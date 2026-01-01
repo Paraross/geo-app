682b5bc8d6ee3d5ac6db7ba1b55c7933ef3e1b95
Paraross <adrago@onet.pl>
2026-01-01 19:15:15 +0100

    Improve task list UI in explore screen

--- scripts/screens/formulas_screen.gd
+++ scripts/screens/formulas_screen.gd
@@ -4,7 +4,9 @@ extends Screen
 # TODO: Fill InfoVbox with task steps
 
 @onready var task_filter_edit: LineEdit = $PanelContainer/HBox/TaskFilterVBox/TaskFilterEdit
-@onready var task_list: ItemList = $PanelContainer/HBox/TaskFilterVBox/TaskList
+@onready var easy_task_list: ItemList = $PanelContainer/HBox/TaskFilterVBox/EasyTaskList
+@onready var medium_task_list: ItemList = $PanelContainer/HBox/TaskFilterVBox/MediumTaskList
+@onready var hard_task_list: ItemList = $PanelContainer/HBox/TaskFilterVBox/HardTaskList
 
 @onready var area_tip_label: Label = $PanelContainer/HBox/InfoVBox/AreaTipLabel
 @onready var volume_tip_label: Label = $PanelContainer/HBox/InfoVBox/VolumeTipLabel
@@ -15,39 +17,50 @@ extends Screen
 
 func on_entered() -> void:
 	task_filter_edit.text = ""
-	fill_task_list()
+	fill_task_lists()
 
 
 func on_left() -> void:
 	task_environment.unload_current_task()
 
 
-func fill_task_list() -> void:
-	task_list.clear()
+func fill_task_lists() -> void:
+	easy_task_list.clear()
+	medium_task_list.clear()
+	hard_task_list.clear()
+
 	for task_name in Tasks.all_tasks:
-		task_list.add_item(task_name)
+		var task := Tasks.all_tasks[task_name]
+		match task.difficulty():
+			Global.TaskDifficulty.EASY:
+				easy_task_list.add_item(task_name)
+			Global.TaskDifficulty.MEDIUM:
+				medium_task_list.add_item(task_name)
+			Global.TaskDifficulty.HARD:
+				hard_task_list.add_item(task_name)
 
 
-func fill_task_list_if(condition: Callable) -> void:
-	task_list.clear()
+func fill_task_lists_if(condition: Callable) -> void:
+	easy_task_list.clear()
+	medium_task_list.clear()
+	hard_task_list.clear()
+
 	for task_name in Tasks.all_tasks:
 		var condition_met: bool = condition.call(task_name)
-		if condition_met:
-			task_list.add_item(task_name)
+		if not condition_met:
+			continue
+
+		var task := Tasks.all_tasks[task_name]
+		match task.difficulty():
+			Global.TaskDifficulty.EASY:
+				easy_task_list.add_item(task_name)
+			Global.TaskDifficulty.MEDIUM:
+				medium_task_list.add_item(task_name)
+			Global.TaskDifficulty.HARD:
+				hard_task_list.add_item(task_name)
 
 
-func _on_task_filter_edit_text_changed(new_text: String) -> void:
-	if new_text.is_empty():
-		fill_task_list()
-		return
-
-	var task_name_contains_filter_text := func(task_name: String) -> bool:
-		return task_name.to_lower().contains(new_text.to_lower())
-
-	fill_task_list_if(task_name_contains_filter_text)
-
-
-func _on_task_list_item_selected(index: int) -> void:
+func on_task_list_item_selected(task_list: ItemList, index: int) -> void:
 	var task_name := task_list.get_item_text(index)
 	var task := Tasks.all_tasks[task_name]
 
@@ -75,3 +88,32 @@ func _on_task_list_item_selected(index: int) -> void:
 		spin_box.value = value_value.value
 		spin_box.value_changed.connect(a)
 		task_data_grid.add_child(spin_box)
+
+
+func _on_task_filter_edit_text_changed(new_text: String) -> void:
+	if new_text.is_empty():
+		fill_task_lists()
+		return
+
+	var task_name_contains_filter_text := func(task_name: String) -> bool:
+		return task_name.to_lower().contains(new_text.to_lower())
+
+	fill_task_lists_if(task_name_contains_filter_text)
+
+
+func _on_easy_task_list_item_selected(index: int) -> void:
+	on_task_list_item_selected(easy_task_list, index)
+	medium_task_list.deselect_all()
+	hard_task_list.deselect_all()
+
+
+func _on_medium_task_list_item_selected(index: int) -> void:
+	on_task_list_item_selected(medium_task_list, index)
+	easy_task_list.deselect_all()
+	hard_task_list.deselect_all()
+
+
+func _on_hard_task_list_item_selected(index: int) -> void:
+	on_task_list_item_selected(hard_task_list, index)
+	easy_task_list.deselect_all()
+	medium_task_list.deselect_all()
