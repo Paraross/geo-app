ae08d961a3ba487e285fe8abff61b2a77ba290c0
Paraross <adrago@onet.pl>
2026-01-15 17:30:00 +0100

    Fix settings constants naming

--- scripts/global/settings.gd
+++ scripts/global/settings.gd
@@ -1,27 +1,27 @@
 extends Node
 
-const config_file_path: String = "user://settings.cfg"
-const settings_section: String = "settings"
+const CONFIG_FILE_PATH: String = "user://settings.cfg"
+const SETTINGS_SECTION: String = "settings"
 
-const data_precision_str: String = "data_precision"
-const answer_precision_str: String = "answer_precision"
+const DATA_PRECISION_STR: String = "data_precision"
+const ANSWER_PRECISION_STR: String = "answer_precision"
 
 var settings: Dictionary[String, Setting] = {
-	data_precision_str: IntSetting.new(1, 0, 6, "", "digits"),
-	answer_precision_str: IntSetting.new(2, 0, 6, "", "digits"),
+	DATA_PRECISION_STR: IntSetting.new(1, 0, 6, "", "digits"),
+	ANSWER_PRECISION_STR: IntSetting.new(2, 0, 6, "", "digits"),
 }
 
 var data_precision: int:
 	get:
-		return settings[data_precision_str].value
+		return settings[DATA_PRECISION_STR].value
 	set(value):
-		settings[data_precision_str].set_value(value)
+		settings[DATA_PRECISION_STR].set_value(value)
 
 var answer_precision: int:
 	get:
-		return settings[answer_precision_str].value
+		return settings[ANSWER_PRECISION_STR].value
 	set(value):
-		settings[answer_precision_str].set_value(value)
+		settings[ANSWER_PRECISION_STR].set_value(value)
 
 
 func _ready() -> void:
@@ -30,7 +30,7 @@ func _ready() -> void:
 
 func load_settings_from_file() -> void:
 	var config := ConfigFile.new()
-	var err := config.load(config_file_path)
+	var err := config.load(CONFIG_FILE_PATH)
 
 	if err == Error.ERR_FILE_NOT_FOUND:
 		save_settings_to_file()
@@ -41,16 +41,16 @@ func load_settings_from_file() -> void:
 		return
 
 	for setting_name in settings:
-		var has_key := config.has_section_key(settings_section, setting_name)
+		var has_key := config.has_section_key(SETTINGS_SECTION, setting_name)
 		if has_key:
-			settings[setting_name].set_value_no_change(config.get_value(settings_section, setting_name))
+			settings[setting_name].set_value_no_change(config.get_value(SETTINGS_SECTION, setting_name))
 
 
 func save_settings_to_file() -> void:
 	var config := ConfigFile.new()
 	for setting_name in settings:
-		config.set_value(settings_section, setting_name, settings[setting_name].value)
-	config.save(config_file_path)
+		config.set_value(SETTINGS_SECTION, setting_name, settings[setting_name].value)
+	config.save(CONFIG_FILE_PATH)
 
 
 func set_all_original_values() -> void:
