26f82bb70dd54f6cb0392d5a6c3b30c551c56b9f
Paraross <adrago@onet.pl>
2026-01-18 13:46:04 +0100

    Make settings screen use the nav bar

--- scripts/screens/settings_screen.gd
+++ scripts/screens/settings_screen.gd
@@ -1,15 +1,7 @@
 class_name SettingsScreen
 extends Screen
 
-signal left(to_main_menu: bool)
-
-var return_to_main_menu: bool
-
-@onready var settings_grid: GridContainer = $CenterContainer/PanelContainer/VBoxContainer/Grid
 @onready var settings_container: VBoxContainer = $CenterContainer/PanelContainer/VBoxContainer/SettingsContainer
-
-@onready var confirm_popup: PopupPanel = $ConfirmPopup
-@onready var button_hbox: HBoxContainer = confirm_popup.get_node("VBox/ButtonHBox")
 
 
 func on_entered() -> void:
@@ -18,7 +10,8 @@ func on_entered() -> void:
 
 
 func on_left() -> void:
-	pass
+	if did_settings_change():
+		Settings.save_settings_to_file()
 
 
 func fill_settings_grid() -> void:
@@ -52,39 +45,7 @@ func fill_settings_grid() -> void:
 		category_container.add_child(setting_ui_element)
 
 
-func settings_changed() -> bool:
+func did_settings_change() -> bool:
 	var is_changed := func(setting: Setting) -> bool: return setting.changed
 	var settings_changed := Settings.settings.values().any(is_changed)
 	return settings_changed
-
-
-func leave_to_last() -> void:
-	leave_screen(false)
-
-
-func leave_to_main_menu() -> void:
-	leave_screen(true)
-
-
-func leave_screen(to_main_menu: bool) -> void:
-	if settings_changed():
-		return_to_main_menu = to_main_menu
-		confirm_popup.show()
-	else:
-		left.emit(to_main_menu)
-
-
-func _on_yes_button_pressed() -> void:
-	Settings.save_settings_to_file()
-	confirm_popup.hide()
-	left.emit(return_to_main_menu)
-
-
-func _on_no_button_pressed() -> void:
-	Settings.reset_all_to_original()
-	confirm_popup.hide()
-	left.emit(return_to_main_menu)
-
-
-func _on_cancel_button_pressed() -> void:
-	confirm_popup.hide()
